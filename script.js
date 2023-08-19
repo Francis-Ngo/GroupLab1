91bc62acdf6b0f25365c427d71ac5117101eaada
czhu-24 <blue.ink.stains@gmail.com>
2023-08-18 17:01:36 -0700

    finished up the extra credit three layered console log yikes

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ let secondVariable = firstVariable;
 secondVariable = 'Im hungry'
 // 6.
 console.log(firstVariable);
-//Should be 123 **TO DO FIX THIS AREA**
+// It'll be 123
 // 7. 
 let yourName = 'BlackPink';
 let expression = 'Hello, my name is ';
@@ -436,30 +436,33 @@ const cat2 = { name: "papaCat", breed: "Egyptian", age: 3 };
 console.log(cat2.breed);
 console.log(cat2.age);
 
-
-// Define combineCats function
+// CombineCats2 function (it won't run if we use combineCats because it is already declared)
 const combineCats = (mama, papa) => {
-    console.log("Mama Cat", mama); 
-    console.log("Papa Cat", papa); 
+    const combinedCat = {
+        name: mama.name + papa.name,
+        age: 1,
+        breed: mama.breed + "-" + papa.breed
+    };
+    return combinedCat;
 };
 
 // Call combineCats function with cat1 and cat2 as arguments
 combineCats(cat1, cat2);
 console.log(cat1, cat2);
 
-// CombineCats2 function (it won't run if we use combineCats because it is already declared)
-const combineCats2 = (mama, papa) => {
-    const combinedCat2 = {
-        name: mama.name + papa.name,
-        age: 1,
-        breed: mama.breed + "-" + papa.breed
-    };
-    return combinedCat2;
-};
-
-// Call combineCats function with cat1 and cat2 as arguments
-const combinedResult = combineCats2(cat1, cat2);
-console.log(combinedResult);
+// calling combineCats with three layers 😱
+console.log(
+    combineCats(
+      combineCats(
+        combineCats(cat1, cat2),
+        combineCats(cat1, cat2)
+      ),
+      combineCats(
+        combineCats(cat1, cat2),
+        combineCats(cat1, cat2)
+      )
+    )
+  );
 
 
 
